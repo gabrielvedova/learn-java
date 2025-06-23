5abc296c40ac9079c3bedbd712be45cbfc6d1d27
gabrielvedova <bielvedova@gmail.com>
2025-06-23 20:22:56 -0300

    feat: add initial project structure and arithmetic operations example

---
+++ arithmetic/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+            Arithmetic Operators in Java
+            int x = 10;
+            int y = 3;
+            int z;
+
+            z = x + y; // Addition
+            z = x - y; // Subtraction
+            z = x * y; // Multiplication
+            z = x / y; // Division
+            z = x % y; // Modulus (Remainder)
+
+            x += y; // Increment x by y (x = x + y)
+            x++ // Post-increment (x = x + 1)
+
+         */
+    }
+}
